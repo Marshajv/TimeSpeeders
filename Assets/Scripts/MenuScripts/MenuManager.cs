ba4af68720158cc070274255913053602b3cf255
Marshajv <Marshajv@gmatc.matc.edu>
2017-04-12 17:17:50 -0500

    added menu

---
+++ Assets/Scripts/MenuScripts/MenuManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+    public void StartGameBtn(string vikingMode)
+    {
+        SceneManager.LoadScene(vikingMode);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
